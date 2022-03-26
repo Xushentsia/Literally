8023873f7c586caa529dd867ca4d4ffab29a8fde
konkoff <xushentsia@mail.ru>
2022-03-26 16:20:36 +0600

    First init (26.03)

---
+++ desktop/src/ru/konkoff/literaly/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package ru.konkoff.literaly.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import ru.konkoff.literaly.MyGdxGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new MyGdxGame(), config);
+	}
+}
